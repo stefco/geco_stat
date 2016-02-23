52baaea60b650aeb135d6500f29f6989ab55b9da
stc <stc@pg1.phys.columbia.edu>
2016-02-23 17:39:31 -0500

    FINISH setup.py FINISH setup_virtualenv.sh

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-"""A setuptools based setup module.
+"""A setuptools based setup module. Copied wholesale from
+https://github.com/pypa/sampleproject
+and edited.
 
 See:
 https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
@@ -11,10 +12,12 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+import geco_statistics
+
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -23,17 +26,17 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',
+    version=geco_statistics.RELEASE,
 
     description='A sample Python project',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/stefco/geco_statistics',
 
     # Author details
-    author='The Python Packaging Authority',
-    author_email='pypa-dev@googlegroups.com',
+    author='Stefan Countryman',
+    author_email='stefan.countryman@ligo.org',
 
     # Choose your license
     license='MIT',
@@ -44,11 +47,11 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Pre-Alpha',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
@@ -66,50 +69,50 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='sample setuptools development',
+    keywords='lsc ligo geco columbia timing diagnostic gravitational waves frame',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #   py_modules=["my_module"],
+    py_modules=["geco_statistics"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],
+    install_requires=['numpy','hdf5','matplotlib'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'sample': ['package_data.dat'],
-    },
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
+    # data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
 )
--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
-virtualenv env
+# Configure a virtualenv for this project.
+
+# Safety first, then teamwork
+set -o errexit
+set -o nounset
+set -o noclobber
+
+# Install a virtualenv to allow for local development and usage.
+# Requires virtualenv if env/ directory does not exist already.
+if ! [ -e "./env" ]
+then
+    virtualenv env
+fi
+
+# Install some stuff required for pip packaging and development
+env/bin/pip install -U "pip>=1.4" "setuptools>=0.9" "wheel>=0.21" twine
+
+# Install required packages into the virtualenv
+if [ -e "requirements.txt" ]
+then
+    printf "Installing packages from requirements.txt...\n"
+    env/bin/pip install -r requirements.txt
+else
+    printf "No requirements.txt file found, moving on...\n"
+fi
+
+cat <<__EOF__
+
+Done setting up! Run
+
+    source env/bin/activate
+
+to start working in this virtual environment, and run
+
+    deactivate
+
+when finished to return to your normal setup.
+
+For nice documentation on virtualenv, visit:
+https://www.dabapps.com/blog/introduction-to-pip-and-virtualenv-python/
+__EOF__

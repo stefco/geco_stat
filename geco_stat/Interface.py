4e470e85537357d91148e15b23906be850346828
Stefan Countryman <stefan.countryman@gmail.com>
2016-03-09 14:40:38 -0500

    FIX vague interfaces

--- geco_stat/Interface.py
+++ geco_stat/Interface.py
@@ -4,26 +4,209 @@ import os
 import h5py             # >=2.5.0
 import abc
 import numpy as np      # >=1.10.4
-from geco_stat._version import __version__, __release__
+from geco_stat._version import __version__
 from geco_stat._constants import __default_bitrate__
 
-
-class ReportInterface(object):
-    "Abstract interface used by all geco_statistics classes"
+# TODO: Implement.
+class AbstractTimeIntervalSet(object):
+    """
+    The AbstractTimeIntervalSet defines what objects can specify time intervals
+    over which data might be collected. In practice, it should only have one
+    implementation; the abstract class serves mainly to keep the type graph
+    acyclic and restrictive.
+    """
     __metaclass__  = abc.ABCMeta
     __version__ = __version__
 
+# TODO: Implement.
+class AbstractTimeSeries(object):
+    """
+    The AbstractTimeSeries provides a way of defining the category of objects
+    which can serve as valid sources of information for geco_stat. In
+    practice, it should only have one implementation; the abstract class serves
+    mainly to keep the type graph acyclic.
+    """
+    __metaclass__  = abc.ABCMeta
+    __version__ = __version__
+
+class TimeSeriesDerivableInterface(object):
+    """
+    The purpose of ``geco_stat`` is to look at data and do things with it,
+    so it is fitting that the primordial interface provides methods for
+    creating a new instance using a Timeseries.
+    """
+    __metaclass__  = abc.ABCMeta
+    __version__ = __version__
+
+    @abc.abstractmethod
+    def clone(self):
+        """
+        Create and return a new instance of this class which shares all
+        values (and which would return true in an equality test) with this
+        instance, but which shares no object pointers (even in sub-properties)
+        and can hence be modified with impunity without fear of side-effects.
+        """
+
+    def get_times(self):
+        """
+        Get an AbstractTimeIntervalSet instance representing the time intervals
+        covered by this instance.
+        """
+        # If storing times differently than suggested here, implement a
+        # different method. But using ``_times`` seems reasonable.
+        if isinstance(self, AbstractTimeIntervalSet):
+            return self.clone()
+        elif isinstance(self._times, AbstractTimeIntervalSet):
+            return self._times.clone()
+
+    @classmethod
+    @abc.abstractmethod
+    def from_timeseries(cls, timeseries):
+        """
+        Create a compatible object using an AbstractTimeSeries as input.
+        """
+
+class NonIntersectingUnionableInterface(TimeSeriesDerivableInterface):
+    """
+    An interface describing classes whose members contain information that is
+    associated with specific time intervals. These members can be unioned or
+    "added" together provided that they don't contain redundant time
+    information. This makes it possible to iteratively build instances of
+    NonIntersectingUnionableInterface that cover large, possibly disjoint
+    spans of time from smaller "atomic" instances simply by unioning.
+    """
+    __metaclass__  = abc.ABCMeta
+    __version__ = __version__
+
+    # FIXME: This is a hack. This should be an abstract class, but most
+    # subclasses have implemented the private methods, so this is a temporary
+    # kludge.
     def union(self, other):
-        "Aggregate these two instances. Must be of compatible type."
-        self._assert_self_consistent()
-        other._assert_self_consistent()
-        self._confirm_unionability(other)
+        """
+        Combine these two instances to cover a larger span of time. Like the
+        ``clone`` method, this should return an instance sharing no object
+        pointers with the original instances.
+        """
         return self.__union__(other)
 
+    # There is no reason to check for consistency every time; too much
+    # abstraction with no clarifying purpose.
+    # FIXME deprecated
+    @abc.abstractmethod
+    def __union__(self, other):
+        """
+        Aggregate these two instances without first checking that the instances
+        are compatible or self-consistent. This is part of the implementation
+        of the union method.
+        """
+
+
+    @abc.abstractmethod
+    def __eq__(self, other):
+        """
+        Instances must have a way of determining equality that is based on
+        values rather than ids, i.e. if the contents of two instances contain
+        the same information, then they are equal, regardless of whether
+        they are the same instance in memory.
+        """
+
+    def __ne__(self, other):
+        return not self == other
+
+
+    def __add__(self, other):
+        """
+        Addition is a shorthand for union.
+        """
+        return self.union(other)
+
+    # TODO Decide what to do about these consistency checks. Maybe implement
+    # a very simple top-level check that can be overridden or extended as
+    # needed, but only if it is considered important; this should not distract.
+    # In any case, the private method name is undesirable and should be
+    # replaced with something better. Not deprecated -- for now.
+    # ALMOST deprecated...
+    @abc.abstractmethod
+    def _assert_self_consistent(self):
+        """
+        Make sure this instance is self-consistent.
+        """
+
+class DictRepresentableInterface(NonIntersectingUnionableInterface):
+    """
+    Subclasses of DictRepresentableInterface contain information that can be
+    represented as a **PARTICULAR** type of dictionary. To be specific, they
+    can be represented as dictionaries whose elements are one of:
+
+    1. A string.
+    2. A numpy.int64.
+    3. A numpy.float64.
+    4. A numpy.ndarray.
+    5. A DictRepresentableInterface instance.
+
+    This allows for unambiguous file saving and loading through the uniform
+    intermediate step of a dictionary representation.
+    """
+    __metaclass__  = abc.ABCMeta
+    __version__ = __version__
+
+    # FIXME: This is a hack. This should be an abstract class, but most
+    # subclasses have implemented the private methods, so this is a temporary
+    # kludge.
+    def to_dict(self):
+        """
+        Return a dictionary whose elements consist of strings, ints, lists, or
+        numpy.ndarray objects, or of other dicts whose contents follow this
+        pattern recursively. This dictionary must wholly represent the data in
+        this object, so that this object may be totally reconstructed using
+        the dictionary's contents. This is an implementation method used to
+        store data in HDF5.
+        """
+        return self.__to_dict__()
+
+    # FIXME: This is a hack. This should be an abstract class, but most
+    # subclasses have implemented the private methods, so this is a temporary
+    # kludge.
+    def from_dict(cls, d):
+        """
+        Construct an instance of this class using a dictionary of the form
+        output by self.__to_dict__. Should generally be a class method.
+        """
+        return cls.__from_dict__t(d)
+
     def clone(self):
-        "Create a new object that is an exact copy of this instance."
-        self._assert_self_consistent()
-        return self.__clone__()
+        """
+        Create and return a new instance of this class which shares all
+        values (and which would return true in an equality test) with this
+        instance, but which shares no object pointers (even in sub-properties)
+        and can hence be modified with impunity without fear of side-effects.
+        """
+        # If this implementation makes you nervous (for anything besides
+        # performance reasons, then you are not properly implementing the
+        # to_dict and from_dict methods.
+        return self.__from_dict__(self.__to_dict__())
+
+    # This interface should become "to_dict", since it will likely be
+    # useful for users.
+    # FIXME deprecated
+    @abc.abstractmethod
+    def __to_dict__(self):
+        pass
+
+    # This interface should become "from_dict", since it will likely be
+    # useful for users.
+    # FIXME deprecated
+    @abc.abstractmethod
+    def __from_dict__(cls, d):
+        pass
+
+class HDF5_IO(DictRepresentableInterface):
+    """
+    Uses the ``to_dict`` methods of DictRepresentableInterface to save and load
+    HDF5 files of arbitrary complexity.
+    """
+    __metaclass__  = abc.ABCMeta
+    __version__ = __version__
 
     def save_hdf5(self, filename):
         """Save this instance to an hdf5 file."""
@@ -119,76 +302,17 @@ class ReportInterface(object):
                     h5file, path + key + '/')
         return ans
 
-    @abc.abstractmethod
-    def from_timeseries(self, timeseries):
-        """
-        Create a compatible object using timeseries data as input. This is
-        an instance method, meant to create a new object compatible with
-        the current instance; this approach allows for a trivially
-        simple method interface, requiring only the timeseries array itself
-        as input.
+# Rename ReportInterface; it should be called something more
+# usage-agnostic and feature-descriptive.
+# FIXME deprecated
+class ReportInterface(HDF5_IO):
+    "Abstract interface used by all geco_statistics classes"
+    __metaclass__  = abc.ABCMeta
+    __version__ = __version__
 
-        The timeseries argument can be composed of multiple rows, representing
-        multiple seconds, worth of data. It can also simply consist of an
-        integer number of seconds worth of data. Consequently, the length of
-        the (flattened) input timeseries must be an integer multiple of the
-        bitrate. This is tested by the abstract method; subclasses can
-        extend it.
-        """
-        l = len(timeseries.flatten())
-        assert l % self.bitrate == 0, ('Flattened timeseries length must be '
-                                       'integer mult of bitrate.')
-        assert l > 0, 'Cannot pass an empty timeseries.'
-
-    @abc.abstractmethod
-    def __to_dict__(self):
-        """
-        Return a dictionary whose elements consist of strings, ints, lists, or
-        numpy.ndarray objects, or of other dicts whose contents follow this
-        pattern recursively. This dictionary must wholly represent the data in
-        this object, so that this object may be totally reconstructed using
-        the dictionary's contents. This is an implementation method used to
-        store data in HDF5.
-        """
-
-    @abc.abstractmethod
-    def __from_dict__(cls, d):
-        """
-        Construct an instance of this class using a dictionary of the form
-        output by self.__to_dict__. Should generally be a class method.
-        """
-
-    @abc.abstractmethod
-    def __clone__(self):
-        """
-        Create a new object that is an exact copy of this instance without
-        first checking for self-consistency. This is part of the implementation
-        of the clone method.
-        """
-
-    @abc.abstractmethod
-    def __union__(self, other):
-        """
-        Aggregate these two instances without first checking that the instances
-        are compatible or self-consistent. This is part of the implementation
-        of the union method.
-        """
-
+    # This should be handled through ducktyping.
+    # FIXME deprecated
     @abc.abstractmethod
     def _confirm_unionability(self, other):
         "Make sure these two instances can be unioned."
 
-    @abc.abstractmethod
-    def _assert_self_consistent(self):
-        "Make sure this instance is self-consistent."
-
-    @abc.abstractmethod
-    def __eq__(self, other):
-        "Instances must have a way of determining equality."
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __add__(self, other):
-        'Addition can be used as a shorthand for union.'
-        return type(self).union(self, other)
